5755aed67c12939a8717739694d13a04cee190c4
Josep1992 <josep@eventray.com>
2020-11-03 02:34:55 -0400

    toggle todos

--- server/database.py
+++ server/database.py
@@ -6,13 +6,17 @@ from constants import CONSTANTS
 
 database = join(CONSTANTS['ROOT'], 'db.json')
 
+def dump(data,file):
+    json.dump(data,file,indent=2,sort_keys=True)
+
+
 def initialize():
     # create db.json file in ./server
     if(os.path.exists(database) == False):
         with open(database,"a+") as file:
             db = {}
             db["todos"] = {}
-            json.dump(db,file,indent=2,sort_keys=True)
+            dump(db,file)
     pass
 
 def create_todo(payload):
@@ -22,12 +26,12 @@ def create_todo(payload):
                 obj = json.load(file)
                 obj["todos"][payload["id"]] = payload
                 with open(database, "w") as f:
-                    json.dump(obj,f,indent=2,sort_keys=True)
+                    dump(obj,f)
                     # return todo with @id and @date
                 return obj["todos"][payload["id"]]
             except Exception as e:
                 print("APPENDING TO FILE FAILED todo:{} exception: {}".format(payload["todo"],e))
-            
+
 
 def delete_todo(id):
     if(os.path.exists(database)):
@@ -37,10 +41,28 @@ def delete_todo(id):
                 if id in obj["todos"]:
                     del obj["todos"][id]
                 with open(database, "w") as f:
-                    json.dump(obj,f,indent=2,sort_keys=True)
-                
+                    dump(obj,f)
+
             except Exception as e:
                 print("DELETE id:{} FAILED exception: {}".format(id,e))
+
+
+def update_todo(payload):
+    if(os.path.exists(database)):
+        with open(database) as file:
+            try:
+                obj = json.load(file)
+                if payload["id"] in obj["todos"]:
+                    with open(database, "w") as f:
+                        obj["todos"][payload["id"]] =  {
+                            **obj["todos"][payload["id"]],
+                            **payload
+                        }
+                        dump(obj,f)
+                        return obj["todos"][payload["id"]] # return the modified todo
+
+            except Exception as e:
+                print("PATCH id:{} FAILED exception: {}".format(payload["id"],e))
 
 
 def todos():
--- server/index.py
+++ server/index.py
@@ -5,7 +5,7 @@ from flask.json import jsonify
 from uuid import uuid4
 from os.path import join
 import database
-from database import create_todo
+from database import create_todo,update_todo
 
 from constants import CONSTANTS
 
@@ -17,37 +17,50 @@ database.initialize()
 def index(path): return send_file(join(app.static_folder, 'index.html'))
 
 
-@app.route('/todos',methods=["GET"])
+@app.route('/api/todos',methods=["GET"])
 def todos():
     payload = database.todos()
-    response = [payload["todos"][id] for id in payload["todos"]]    
-    return jsonify({"todos":response})
+    response = [payload["todos"][id] for id in payload["todos"]]
+    return jsonify({"todos":response,"success":True})
 
 
-@app.route('/add',methods=["POST"])
+@app.route('/api/add',methods=["POST"])
 def add_todo():
     payload = request.get_json(request.data)
 
     if payload['todo'] == '':
-        return jsonify({"error": "Todo's can't be empty"})
+        return jsonify({"error": "Todo nothing really?!","success": False})
 
     payload['id'] = str(uuid4())
     payload['date'] = date.isoformat(date.today())
 
     todo = create_todo(payload)
 
-    return jsonify({"todo": todo})
+    return jsonify({"todo": todo,"success":True})
 
-@app.route('/delete',methods=["DELETE"])
+
+@app.route('/api/edit',methods=["PUT"])
+def edit_todo():
+    payload = request.get_json(request.data)
+
+    if payload['id'] == '':
+        return jsonify({"error": "id not found","success": False})
+
+    todo = update_todo(payload)
+
+    return jsonify({"todo": todo,"success":True})
+
+
+@app.route('/api/delete',methods=["DELETE"])
 def delete_todo():
     payload = request.get_json(request.data)
 
     if payload['id'] == "":
-        return jsonify({"error": "id is empty"})
+        return jsonify({"error": "id not found","success":False})
 
     database.delete_todo(payload["id"])
 
-    return jsonify({"message": 'Todo Deleted', "id": payload["id"]})
+    return jsonify({"message": 'Todo Deleted', "id": payload["id"],"success":True})
 
 
 if __name__ == "__main__":
