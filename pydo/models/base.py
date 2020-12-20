5122a31afcfa88901e469d420d4762815f8d9777
Josep López <josep-lopez@live.com>
2020-12-20 16:06:41 -0400

    :)

--- pydo/models/base.py
+++ pydo/models/base.py
@@ -4,7 +4,6 @@ from datetime import datetime
 class Base(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-    # todos = db.relationship('Todo',backref='todos',lazy=True)
 
     def __repr__(self):
         return f"Base('{self.id}')('{self.date_created}')"
--- pydo/models/users.py
+++ pydo/models/users.py
@@ -2,10 +2,8 @@ from pydo import db
 from pydo.models.base import Base
 
 class Users(Base):
-    # id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False,unique=True)
     password = db.Column(db.String(60), nullable=False)
-    # date_created = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
     # todos = db.relationship('Todo',backref='todos',lazy=True)
 
     def __repr__(self):
